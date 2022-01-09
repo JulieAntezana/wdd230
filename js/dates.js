5d1ca8c2732fa3fd1dda701286d637cac10af6af
JulieAntezana <jantezana@byui.edu>
2022-01-09 06:54:56 -0600

    Update #5

--- js/dates.js
+++ js/dates.js
@@ -1,10 +1,8 @@
-
-//const year = d.getFullYear();
+// declarations
+const d = new Date();
+const year = d.getFullYear();
 const lastmod = document.querySelector('#lastmod');
-//lastmod.textContent = `Last Update: ${document.lastModified}`;
-
-//lastmod.innerHTML = `<strong>Last UPDATE</strong> last modified: ${document.lastModified}`;
 
 // using querySelector
-//document.querySelector('#currentyear').textContent = year;
+document.querySelector('#currentyear').textContent = year;
 document.querySelector('#lastmod').textContent += `Last Update: ${document.lastModified}`;
