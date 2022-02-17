af5493703c18525252f11e5666e14592ea3903bd
JulieAntezana <jantezana@byui.edu>
2022-02-17 13:01:26 -0600

    Fixed responsive picture sizing

--- Chamber/js/weather.js
+++ Chamber/js/weather.js
@@ -1,12 +1,12 @@
-const temp = parseFloat(document.querySelector("#temp").innerHTML)
-const windspeed = parseFloat(document.querySelector("#windspeed").innerHTML)
+const temp = parseInt(document.querySelector("#temp").textContent);
+const speed = parseInt(document.querySelector("#speed").textContent);
 
 
 
-if(temp <= 50 && windspeed > 3){
-    let windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16));
+if(temp <= 50 && speed > 3){
+    let windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16));
     windchill = Math.round((windchill + Number.EPSILON) * 10) / 10;
-    document.querySelector("#windchill").innerHTML = `${windchill}&#176;F`;
+    document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}`;
 } else{
-    "N/A"
+    document.querySelector("#windchill").innerHTML = "N/A"
 }
