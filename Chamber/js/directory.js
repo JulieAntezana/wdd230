2e6ae6f6f349b7c080d6c8cc2786c3c9581fb743
JulieAntezana <jantezana@byui.edu>
2022-03-05 23:37:40 -0600

    Clean up HTML

--- Chamber/js/directory.js
+++ Chamber/js/directory.js
@@ -1,18 +1,9 @@
 // store resource URL in a const
 const requestURL = "https://julieantezana.github.io/wdd230/Chamber/json/data.json";
 const cards = document.querySelector(".cards");
-const lists = document.querySelector(".lists")
+const lists = document.querySelector(".lists");
 
-// console.log("Load Directory");
-// getDirectory();
-
-// async function getDirectory() {
-//   console.log('get data');
-//   const response = await fetch(requestData, {mode: 'no-cors'});
-//   const business_dir = await response.json();
-//   business_dir.forEach(displayBusiness);
-
-// basic fetch( method) implementation
+// fetch data and return in json format
 fetch(requestURL)
     .then(function (response) {
         return response.json();
@@ -21,65 +12,72 @@ fetch(requestURL)
     .then(function (jsonData) {
         // console.table(jsonData);
         const directoryList = jsonData["directory"];
-
-        const cardView = document.querySelector(".cardButton");
-        const listView = document.querySelector(".listButton");
         // display default view
-        directoryList.forEach(displayDir);
-        // display listView     
-        listView.addEventListener("click", function () {
-            toggleList()
-            directoryList.forEach(displayList);
-        });
-        // display cardView
-        cardView.addEventListener("click", function () {
-            toggleCard()
-            directoryList.forEach(displayDir);
-        });
-    });
+        directoryList.forEach(displayList);
+});
+ 
+    // const cardView = document.querySelector(".cardButton");
+    // const listView = document.querySelector(".listButton");
+
+    // // display listView     
+    // listView.addEventListener("click", function () {
+    //     toggleList()
+    //     directory.forEach(displayList);
+    // });
+    // // display cardView
+    // cardView.addEventListener("click", function () {
+    //     toggleCard()
+    //     directory.forEach(displayDir);
+    // });
+
 
 function displayDir(directory) {
     // create elements to add to the document
     let card = document.createElement("section");
     card.className = "cardSection";
-    let businessname = document.createElement("h2")
-    let logo = document.createElement("img")
-    let address = document.createElement("p")
-    let phone = document.createElement("p")
-    let website = document.createElement("p")
+    let businessname = document.createElement("h2");
+    let logo = document.createElement("img");
+    let address = document.createElement("p");
+    let phone = document.createElement("p");
+    let website = document.createElement("p");
 // add the directory data to the HTML 
     businessname.textContent = directory.businessname;
     address.innerHTML = directory.address;
     phone.innerHTML = directory.phone;
     logo.setAttribute('src', directory.logourl);
     logo.setAttribute('alt', `${directory.businessname} logo`);
-    logo.setAttribute('loading', 'lazy')
+    logo.setAttribute('loading', 'lazy');
     website.setAttribute('class','link'); 
     website.innerHTML = "<a href=" + `${directory.website}` + ">" + `${directory.website}`;
     card.appendChild(logo);
     card.appendChild(businessname);
-    card.appendChild(address)
-    card.appendChild(phone)
-    card.appendChild(website)
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
     cards.appendChild(card);
 }
 
 function displayList(directory) {
     let tr = document.createElement("tr");
-    let website = document.createElement("p")
-    tr.innerHTML = `<td>${directory.businessname} </td> <td>${directory.phone}</td> <td>${directory.address}</td> `
+    let website = document.createElement("p");
+    tr.innerHTML = `<td>${directory.businessname} </td> <td>${directory.phone}</td> <td>${directory.address}</td> `;
     website.setAttribute('class','link'); 
     website.innerHTML = "<a href=" + `${directory.website}` + ">" + `${directory.website}`;
-    tr.appendChild(website)
-    lists.appendChild(tr)
-    
+    tr.appendChild(website);
+    // append the list view
+    lists.appendChild(tr);  
 }
 
-function toggleCard() {
-    document.getElementById("cardToggle").style.display = "grid";
-    document.getElementById("listToggle").style.display = "none";
-}
-function toggleList() {
-    document.getElementById("listToggle").style.display = "block";
-    document.getElementById("cardToggle").style.display = "none";
-}
+// function toggleCard() {
+//     document.getElementById("cardToggle").style.display = "grid";
+//     document.getElementById("listToggle").style.display = "none";
+// }
+// function toggleList() {
+//     document.getElementById("listToggle").style.display = "block";
+//     document.getElementById("cardToggle").style.display = "none";
+// }
+
+function dirButtonToggle() {
+    document.getElementById('directButton').classList.toggle('active');
+    document.getElementById('directContainer').classList.toggle('active');
+  }
